f7a6644709f7ddc6e601867c6e2b149c562da52b
Afeez <Afeezoluwa@yahoo.ie>
2024-03-03 20:48:23 +0000

    hi

--- src/SearchBySurnameDialog.java
+++ src/SearchBySurnameDialog.java
@@ -1,85 +1,63 @@
-/*
- * 
- * This is a dialog for searching Employees by their surname.
- * 
- * */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class SearchBySurnameDialog extends JDialog implements ActionListener {
+    private EmployeeDetails parent;
+    private JButton searchButton, cancelButton;
+    private JTextField searchField;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.border.EtchedBorder;
+    public SearchBySurnameDialog(EmployeeDetails parent) {
+        setTitle("Search by Surname");
+        setModal(true);
+        this.parent = parent;
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-public class SearchBySurnameDialog extends JDialog implements ActionListener{
-	EmployeeDetails parent;
-	JButton search, cancel;
-	JTextField searchField;
-	// constructor for search by surname dialog
-	public SearchBySurnameDialog(EmployeeDetails parent) {
-		setTitle("Search by Surname");
-		setModal(true);
-		this.parent = parent;
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JScrollPane scrollPane = new JScrollPane(createSearchPane());
+        setContentPane(scrollPane);
 
-		JScrollPane scrollPane = new JScrollPane(searchPane());
-		setContentPane(scrollPane);
+        getRootPane().setDefaultButton(searchButton);
 
-		getRootPane().setDefaultButton(search);
-		
-		setSize(500, 190);
-		setLocation(350, 250);
-		setVisible(true);
-	}// end SearchBySurnameDialog
-	
-	// initialize search container
-	public Container searchPane() {
-		JPanel searchPanel = new JPanel(new GridLayout(3,1));
-		JPanel textPanel = new JPanel();
-		JPanel buttonPanel = new JPanel();
-		JLabel searchLabel;
+        setSize(500, 190);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
-		searchPanel.add(new JLabel("Search by Surname"));
-	
-		textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		textPanel.add(searchLabel = new JLabel("Enter Surname:"));
-		searchLabel.setFont(this.parent.font1);
-		textPanel.add(searchField = new JTextField(20));
-		searchField.setFont(this.parent.font1);
-		searchField.setDocument(new JTextFieldLimit(20));
+    private Container createSearchPane() {
+        JPanel searchPanel = new JPanel(new GridLayout(3, 1));
+        JPanel textPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
+        JLabel searchLabel;
 
-		buttonPanel.add(search = new JButton("Search"));
-		search.addActionListener(this);
-		search.requestFocus();
-		
-		buttonPanel.add(cancel = new JButton("Cancel"));
-		cancel.addActionListener(this);
-		
-		searchPanel.add(textPanel);
-		searchPanel.add(buttonPanel);
+        searchPanel.add(new JLabel("Search by Surname"));
 
-		return searchPanel;
-	}// end searchPane
+        textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        textPanel.add(searchLabel = new JLabel("Enter Surname:"));
+        searchLabel.setFont(this.parent.getFont1());
+        textPanel.add(searchField = new JTextField(20));
+        searchField.setFont(this.parent.getFont1());
+        searchField.setDocument(new JTextFieldLimit(20));
 
-	// action listener for save and cancel button
-	public void actionPerformed(ActionEvent e) {
-		// if option search, search for Employee
-		if(e.getSource() == search){
-			this.parent.searchBySurnameField.setText(searchField.getText());
-			// search Employee by surname
-			this.parent.searchEmployeeBySurname();
-			dispose();// dispose dialog
-		}// end if
-		// else dispose dialog
-		else if(e.getSource() == cancel)
-			dispose();// dispose dialog
-	}// end actionPerformed
-}// end class SearchBySurnameDialog
+        buttonPanel.add(searchButton = new JButton("Search"));
+        searchButton.addActionListener(this);
+        searchButton.requestFocus();
+
+        buttonPanel.add(cancelButton = new JButton("Cancel"));
+        cancelButton.addActionListener(this);
+
+        searchPanel.add(textPanel);
+        searchPanel.add(buttonPanel);
+
+        return searchPanel;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchButton) {
+            this.parent.getSearchBySurnameField().setText(searchField.getText());
+            this.parent.searchEmployeeBySurname();
+            dispose();
+        } else if (e.getSource() == cancelButton) {
+            dispose();
+        }
+    }
+}
